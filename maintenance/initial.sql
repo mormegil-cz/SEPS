99be37d27f08d1011bacfe0c7f1e33478e630f16
Petr Kadlec <mormegil@centrum.cz>
2014-08-29 22:58:03 +0200

    Missing semicolon

--- maintenance/initial.sql
+++ maintenance/initial.sql
@@ -1,5 +1,5 @@
 -- admin:h
-INSERT INTO users (username, password, caption, globalrights) VALUES ('admin', 'aaa9402664f1a41f40ebbc52c9993eb66aeb366602958fdfaa283b71e64db123', 'admin', 0x001F)
+INSERT INTO users (username, password, caption, globalrights) VALUES ('admin', 'aaa9402664f1a41f40ebbc52c9993eb66aeb366602958fdfaa283b71e64db123', 'admin', 0x001F);
 
 -- české svátky
 INSERT INTO holidays (country, day, fromyear, toyear) VALUES ('CZ', 0, 1979, 2038);
